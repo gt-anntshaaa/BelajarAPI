79cc58c97ac4af14369e7ea046c3bcd5ad274511
NISA <anastasya.nisa57@gmail.com>
2022-10-29 14:44:34 +0700

    materi api

---
+++ app/src/main/java/com/chirikualii/materiapi/remote/ApiService.kt
@@ -0,0 +1,10 @@
+package com.chirikualii.materiapi.remote
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("movie/now_playing?api_key=3b980052023e8e6898f4578a6972740b&language=en-US")
+    suspend fun getMovies(@Query("page")page: String): Response<NowPlayingResponse>
+}
